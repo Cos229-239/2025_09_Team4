738cdc271b748d9891dbcdf0284051d4c6996f07
Jonny Sueck <jonny.sueck@petco.com>
2025-10-02 16:21:49 -0400

    Added dynamic box to the add screen that upon click, navigates the user to the photo picker. Once the user selects a photo, it will display the image.

--- app/src/main/java/com/kodeco/memeverse/navigation/AppNavHost.kt
+++ app/src/main/java/com/kodeco/memeverse/navigation/AppNavHost.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.kodeco.memeverse.composables.TopBarNavigationDetail
-import com.kodeco.memeverse.screens.AddScreen
+import com.kodeco.memeverse.screens.add.AddScreen
 import com.kodeco.memeverse.screens.FeedScreen
 import com.kodeco.memeverse.screens.GeneratorScreen
 import com.kodeco.memeverse.screens.login.LoginScreen
