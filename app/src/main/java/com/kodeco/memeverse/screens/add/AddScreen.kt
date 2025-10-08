52e354640090743b52b9a6e2af1c8c9b8a938a11
jonathansueck <jonathansueck@gmail.com>
2025-10-08 18:17:51 -0400

    Added in bottom row for the user to select their visibility and a button to proceed to the next screen.

--- app/src/main/java/com/kodeco/memeverse/screens/add/AddScreen.kt
+++ app/src/main/java/com/kodeco/memeverse/screens/add/AddScreen.kt
@@ -6,15 +6,22 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Public
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,8 +71,7 @@ fun AddScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding),
-                horizontalAlignment =  Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Top
+                horizontalAlignment =  Alignment.Start,
             ) {
                 Card(
                     modifier = Modifier
@@ -103,8 +110,62 @@ fun AddScreen(
                             )
                         }
                     }
-
                 }
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(2.dp, 0.dp, 10.dp, 100.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(start = 10.dp, bottom = 20.dp)
+                            .border(1.dp, MaterialTheme.colorScheme.onSurface)
+                            .background(MaterialTheme.colorScheme.primaryContainer)
+                            .align(Alignment.BottomStart)
+                    ) {
+                        Text(
+                            "Everyone",
+                            modifier = Modifier
+                                .padding(start = 5.dp)
+
+                        )
+
+                        Icon(
+                            imageVector = Icons.Filled.Public,
+                            contentDescription = "Public Icon",
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(bottom = 10.dp)
+                            .height(50.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(MaterialTheme.colorScheme.primary.hashCode()),
+                            disabledContainerColor = Color(0xFFB2EBF2)
+                        ),
+                        shape = MaterialTheme.shapes.large
+                    ) {
+                        Text("Next")
+                    }
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .fillMaxWidth(),
+                        thickness = 1.dp,
+                        color = MaterialTheme.colorScheme.primaryContainer
+                    )
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .fillMaxWidth()
+                            .padding(bottom = 65.dp),
+                        thickness = 1.dp,
+                        color = MaterialTheme.colorScheme.primaryContainer
+                    )
+                }
+
             }
         }
     }
