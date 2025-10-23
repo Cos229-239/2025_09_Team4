bd6261dc887b64c417d67ee7cbe43d9ffd5b7103
jonathansueck <jonathansueck@gmail.com>
2025-10-22 20:45:07 -0400

    Renamed 'FeedPage' to 'FeedScreen' to conform with naming convention.

--- app/src/main/java/com/kodeco/memeverse/screens/FeedScreen.kt
+++ app/src/main/java/com/kodeco/memeverse/screens/FeedScreen.kt
@@ -1,7 +1,6 @@
 package com.kodeco.memeverse.screens
 
 //Libraries
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
